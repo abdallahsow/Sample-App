514c822ebbc14d623645c487a239d0a4f2343cfe
Abdallah Sow <abdallahsow700@gmail.com>
2022-07-17 13:56:14 +0300

    Change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Simple demo of React app
+          Demo of React app
         </p>
         <a
           className="App-link"
