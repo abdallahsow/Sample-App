7f30dd6608551ed8209b62a76c7c760e73681c88
Abdallah Sow <abdallahsow700@gmail.com>
2023-01-05 17:59:26 +0300

    input basic

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
+// Basic Types
 let id = 5;
+let company = "Traversy Media";
+let isPublished = true;
+let x = "Hello";
+x = 25541;
+console.log("ID: " + id);
+let institutions = ["Traversy Media", "Udemy", "Coursera"];
+institutions.push("254");
+let arr = [1, true, "Hello"];
+// Tuple
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+// Basic Types
 let id: number = 5;
+let company: string = "Traversy Media";
+let isPublished: boolean = true;
+let x: any = "Hello";
+
+x = 25541;
+
+console.log("ID: " + id);
+
+let institutions: string[] = ["Traversy Media", "Udemy", "Coursera"];
+institutions.push("254");
+
+let arr: any[] = [1, true, "Hello"];
+
+// Tuple
