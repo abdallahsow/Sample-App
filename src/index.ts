04612ffba3d314c8ba7954e105e956b5a6224730
Abdallah Sow <abdallahsow700@gmail.com>
2023-01-06 09:59:24 +0300

    Tuples introduction

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,12 @@ institutions.push("254");
 let arr: any[] = [1, true, "Hello"];
 
 // Tuple
+let person: [number, string, boolean] = [1, "Brad:", true];
+
+let employee: [number, string][];
+
+employee = [
+  [1, "Brad"],
+  [2, "John"],
+  [3, "Jill"],
+];
