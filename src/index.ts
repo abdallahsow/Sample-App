59dc1328c93ef7eaab90bae763572a881602d43a
Abdallah Sow <abdallahsow700@gmail.com>
2023-01-15 20:58:04 +0300

    Added an interface MathFunc

--- src/index.ts
+++ src/index.ts
@@ -83,12 +83,21 @@ log("Hello World");
 
 // Interfaces
 interface UserInterface {
-    readonly id: number;
-    name: string;
-    age?: number;
+  readonly id: number;
+  name: string;
+  age?: number;
 }
 
 const user1: UserInterface = {
-    id: 1,
-    name: "John",
+  id: 1,
+  name: "John",
+};
+
+interface MathFunc {
+  (x: number, y: number): number;
 }
+
+const add: MathFunc = (x: number, y: number): number => x + y;
+const sub: MathFunc = (x: number, y: number): number => x - y;
+const mul: MathFunc = (x: number, y: number): number => x * y;
+const div: MathFunc = (x: number, y: number): number => x / y;
