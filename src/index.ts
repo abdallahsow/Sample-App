5cc8006b589e433315d37017fdf95c4c5511f1bd
Abdallah Sow <abdallahsow700@gmail.com>
2023-01-16 17:26:24 +0300

    Removed an interface MathFunc

--- src/index.ts
+++ src/index.ts
@@ -93,11 +93,3 @@ const user1: UserInterface = {
   name: "John",
 };
 
-interface MathFunc {
-  (x: number, y: number): number;
-}
-
-const add: MathFunc = (x: number, y: number): number => x + y;
-const sub: MathFunc = (x: number, y: number): number => x - y;
-const mul: MathFunc = (x: number, y: number): number => x * y;
-const div: MathFunc = (x: number, y: number): number => x / y;
